5dee26d0b9b799b47e61a3e97c8eab4a40dcfc8d
Kadir Kağan Yüksel <101135338+Kadiryuksell@users.noreply.github.com>
2022-03-07 21:35:40 +0300

    Add files via upload

---
+++ VeriYapilariOdev/VeriYapilariOdev/VeriYapilariOdev/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace VeriYapilariOdev
+{
+    partial class frmPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.basaEkleBtn = new System.Windows.Forms.Button();
+            this.bastanSilBtn = new System.Windows.Forms.Button();
+            this.sonaEkleBtn = new System.Windows.Forms.Button();
+            this.sondanSilBtn = new System.Windows.Forms.Button();
+            this.listeGorBtn = new System.Windows.Forms.Button();
+            this.arayaEkleBtn = new System.Windows.Forms.Button();
+            this.aradanSilBtn = new System.Windows.Forms.Button();
+            this.reverseBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // basaEkleBtn
+            // 
+            this.basaEkleBtn.Location = new System.Drawing.Point(105, 53);
+            this.basaEkleBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.basaEkleBtn.Name = "basaEkleBtn";
+            this.basaEkleBtn.Size = new System.Drawing.Size(171, 31);
+            this.basaEkleBtn.TabIndex = 0;
+            this.basaEkleBtn.Text = "Basa Ekle";
+            this.basaEkleBtn.UseVisualStyleBackColor = true;
+            this.basaEkleBtn.Click += new System.EventHandler(this.basaEkleBtn_Click);
+            // 
+            // bastanSilBtn
+            // 
+            this.bastanSilBtn.Location = new System.Drawing.Point(105, 123);
+            this.bastanSilBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bastanSilBtn.Name = "bastanSilBtn";
+            this.bastanSilBtn.Size = new System.Drawing.Size(171, 31);
+            this.bastanSilBtn.TabIndex = 1;
+            this.bastanSilBtn.Text = "Bastan Sil";
+            this.bastanSilBtn.UseVisualStyleBackColor = true;
+            this.bastanSilBtn.Click += new System.EventHandler(this.bastanSilBtn_Click);
+            // 
+            // sonaEkleBtn
+            // 
+            this.sonaEkleBtn.Location = new System.Drawing.Point(105, 87);
+            this.sonaEkleBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sonaEkleBtn.Name = "sonaEkleBtn";
+            this.sonaEkleBtn.Size = new System.Drawing.Size(171, 31);
+            this.sonaEkleBtn.TabIndex = 2;
+            this.sonaEkleBtn.Text = "Sona Ekle";
+            this.sonaEkleBtn.UseVisualStyleBackColor = true;
+            this.sonaEkleBtn.Click += new System.EventHandler(this.sonaEkleBtn_Click);
+            // 
+            // sondanSilBtn
+            // 
+            this.sondanSilBtn.Location = new System.Drawing.Point(105, 157);
+            this.sondanSilBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sondanSilBtn.Name = "sondanSilBtn";
+            this.sondanSilBtn.Size = new System.Drawing.Size(171, 31);
+            this.sondanSilBtn.TabIndex = 3;
+            this.sondanSilBtn.Text = "Sondan Sil";
+            this.sondanSilBtn.UseVisualStyleBackColor = true;
+            this.sondanSilBtn.Click += new System.EventHandler(this.sondanSilBtn_Click);
+            // 
+            // listeGorBtn
+            // 
+            this.listeGorBtn.Location = new System.Drawing.Point(22, 11);
+            this.listeGorBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listeGorBtn.Name = "listeGorBtn";
+            this.listeGorBtn.Size = new System.Drawing.Size(340, 31);
+            this.listeGorBtn.TabIndex = 4;
+            this.listeGorBtn.Text = "Listeyi Gör";
+            this.listeGorBtn.UseVisualStyleBackColor = true;
+            this.listeGorBtn.Click += new System.EventHandler(this.listeGorBtn_Click);
+            // 
+            // arayaEkleBtn
+            // 
+            this.arayaEkleBtn.Location = new System.Drawing.Point(105, 193);
+            this.arayaEkleBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.arayaEkleBtn.Name = "arayaEkleBtn";
+            this.arayaEkleBtn.Size = new System.Drawing.Size(171, 26);
+            this.arayaEkleBtn.TabIndex = 5;
+            this.arayaEkleBtn.Text = "Araya Ekle";
+            this.arayaEkleBtn.UseVisualStyleBackColor = true;
+            this.arayaEkleBtn.Click += new System.EventHandler(this.arayaEkleBtn_Click);
+            // 
+            // aradanSilBtn
+            // 
+            this.aradanSilBtn.Location = new System.Drawing.Point(105, 224);
+            this.aradanSilBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.aradanSilBtn.Name = "aradanSilBtn";
+            this.aradanSilBtn.Size = new System.Drawing.Size(171, 26);
+            this.aradanSilBtn.TabIndex = 6;
+            this.aradanSilBtn.Text = "Aradan Sil";
+            this.aradanSilBtn.UseVisualStyleBackColor = true;
+            this.aradanSilBtn.Click += new System.EventHandler(this.aradanSilBtn_Click);
+            // 
+            // reverseBtn
+            // 
+            this.reverseBtn.Location = new System.Drawing.Point(105, 257);
+            this.reverseBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.reverseBtn.Name = "reverseBtn";
+            this.reverseBtn.Size = new System.Drawing.Size(171, 27);
+            this.reverseBtn.TabIndex = 7;
+            this.reverseBtn.Text = "Reverse";
+            this.reverseBtn.UseVisualStyleBackColor = true;
+            this.reverseBtn.Click += new System.EventHandler(this.reverseBtn_Click);
+            // 
+            // frmPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 359);
+            this.Controls.Add(this.reverseBtn);
+            this.Controls.Add(this.aradanSilBtn);
+            this.Controls.Add(this.arayaEkleBtn);
+            this.Controls.Add(this.listeGorBtn);
+            this.Controls.Add(this.sondanSilBtn);
+            this.Controls.Add(this.sonaEkleBtn);
+            this.Controls.Add(this.bastanSilBtn);
+            this.Controls.Add(this.basaEkleBtn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmPanel";
+            this.Text = "Panel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button basaEkleBtn;
+        private System.Windows.Forms.Button bastanSilBtn;
+        private System.Windows.Forms.Button sonaEkleBtn;
+        private System.Windows.Forms.Button sondanSilBtn;
+        private System.Windows.Forms.Button listeGorBtn;
+        private System.Windows.Forms.Button arayaEkleBtn;
+        private System.Windows.Forms.Button aradanSilBtn;
+        private System.Windows.Forms.Button reverseBtn;
+    }
+}
+
